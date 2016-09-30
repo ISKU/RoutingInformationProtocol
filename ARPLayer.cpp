139e68bc89d7d11254be125634d4826f893f0ce2
SanggiHong <sanggi.hong11@gmail.com>
2016-09-30 16:35:31 +0900

    Reduce indent of ARP

--- ARPLayer.cpp
+++ ARPLayer.cpp
@@ -8,6 +8,7 @@ CARPLayer::CARPLayer(char *pName) : CBaseLayer(pName)
 	ResetMessage();
 	ResetMessageProxy();
 	buf_index = 0;
+	buf_size = 0;
 	out_index = 0;	
 	buf[0].valid = 0;
 	buf[1].valid = 1;
@@ -23,126 +24,134 @@ BOOL CARPLayer::Send(unsigned char* ppayload, int nlength,int dev_num){
 	unsigned char broadcast[4];
 	memset(broadcast,0xff,4);
 
-	// MH: ARP로 내려온 패킷에서 IP Layer의 Destination IP Address가 Broadcast인 경우에는 ARP Table Search 할 필요 없이 바로 보낸다.
-	if (!memcmp(routerDlg->m_IPLayer->GetDstIP(dev_num), broadcast, 4)) {
-		return routerDlg->m_EthernetLayer->Send(ppayload,nlength,ip_type,dev_num);
+	if(memcmp(routerDlg->m_IPLayer->GetDstIP(dev_num), broadcast, 4) == 0){
+		//broadcast일 경우 바로 보냄
+		return ((CEthernetLayer*)this->mp_UnderLayer)->Send(ppayload,nlength,ip_type,dev_num);
 	}
-	else if((index = SearchIpAtTable(routerDlg->m_IPLayer->GetDstIP(dev_num))) != -1){ //search 결과 존재하는경우.
+	
+	if((index = SearchIpAtTable(routerDlg->m_IPLayer->GetDstIP(dev_num))) != -1){
+		//search 결과 존재하는경우.
 		POSITION pos = Cache_Table.FindIndex(index);
-		if(Cache_Table.GetAt(pos).cache_type == complete){ //해당 결과가 complete일 경우
-			((CEthernetLayer *)GetUnderLayer())->SetDestinAddress(Cache_Table.GetAt(pos).Mac_addr, dev_num); //해당 mac을 설정
-			return routerDlg->m_EthernetLayer->Send(ppayload,nlength,ip_type,dev_num); //그냥 보냄
+		if(Cache_Table.GetAt(pos).cache_type == complete){
+			//해당 결과가 complete일 경우
+			((CEthernetLayer*)this->mp_UnderLayer)->SetDestinAddress(Cache_Table.GetAt(pos).Mac_addr, dev_num); //해당 mac을 설정
+			return ((CEthernetLayer*)this->mp_UnderLayer)->Send(ppayload, nlength, ip_type, dev_num);
 		}
-		else{ //해당결과가 incomplete -- 보내지 않음
-			return false;
-		}
-	} else if(!memcmp(routerDlg->m_IPLayer->GetDstIP(dev_num), routerDlg->m_IPLayer->GetSrcIP(dev_num), 4)){ //자신 ip로 보내는 경우
-		arp_message.arp_op = request; //request message
-		memcpy(arp_message.arp_srcprotoaddr,routerDlg->m_IPLayer->GetSrcIP(dev_num),4); //보내는 사람 ip
-		memcpy(arp_message.arp_destprotoaddr,routerDlg->m_IPLayer->GetDstIP(dev_num),4); //받는사람 ip
-		memcpy(arp_message.arp_srchaddr,routerDlg->m_EthernetLayer->GetSourceAddress(dev_num),6); //보내는 사람 mac
-		return routerDlg->m_EthernetLayer->Send((unsigned char *)&arp_message,ARP_MESSAGE_SIZE,arp_type,dev_num); //gratuitous arp message
-	} else{ //serch 결과 존재하지 않을경우
-		ResetMessage(); //메시지 초기화
-		//buffer에 패킷 저장
-		if(buf[buf_index].valid == 0){ //버퍼가 비어있을 경우
-			buf[buf_index].data = (unsigned char *)malloc(nlength);
-			memcpy(buf[buf_index].data,ppayload,nlength); //패킷 저장
-			buf[buf_index].dev_num = dev_num;
-			buf[buf_index].valid = 1;
-			buf[buf_index].nlength = nlength;
-			memcpy(buf[buf_index].dest_ip,routerDlg->m_IPLayer->GetDstIP(dev_num),4);
-			buf_index++;
-			buf_index %= 2; //Circular 버퍼
-		} //안비어 있다면 패킷을버림
-
-		arp_message.arp_op = request; //request message
-		memcpy(arp_message.arp_srcprotoaddr,routerDlg->m_IPLayer->GetSrcIP(dev_num),4); //보내는 사람 ip
-		memcpy(arp_message.arp_srchaddr,routerDlg->m_EthernetLayer->GetSourceAddress(dev_num),6); //보내는사람 mac
-		memcpy(arp_message.arp_destprotoaddr,routerDlg->m_IPLayer->GetDstIP(dev_num),4); //받는사람 ip
-		//LP_arpDlg->SetTimer(wait_timer,4000,NULL); //timer 가동
-		return routerDlg->m_EthernetLayer->Send((unsigned char *)&arp_message,ARP_MESSAGE_SIZE,arp_type,dev_num);//arp message
+		//해당결과가 incomplete -- 보내지 않음
 	}
-	return FALSE;
+
+	if(memcmp(routerDlg->m_IPLayer->GetDstIP(dev_num), routerDlg->m_IPLayer->GetSrcIP(dev_num), 4) == 0){
+		//자신 ip로 보내는 경우
+		arp_message.arp_op = request; //request message
+		memcpy(arp_message.arp_srcprotoaddr, routerDlg->m_IPLayer->GetSrcIP(dev_num), 4); //보내는 사람 ip
+		memcpy(arp_message.arp_destprotoaddr, routerDlg->m_IPLayer->GetDstIP(dev_num), 4); //받는사람 ip
+		memcpy(arp_message.arp_srchaddr, routerDlg->m_EthernetLayer->GetSourceAddress(dev_num), 6); //보내는 사람 mac
+		return ((CEthernetLayer*)this->mp_UnderLayer)->Send((unsigned char *)&arp_message, ARP_MESSAGE_SIZE, arp_type, dev_num); //gratuitous arp message
+	}
+
+	//아무 정보가 없음
+	ResetMessage(); //메시지 초기화
+	//buffer에 패킷 저장
+	if(buf[buf_index].valid == 0){ //버퍼가 비어있을 경우
+		buf[buf_index].valid = 1;
+		buf[buf_index].data = (unsigned char *) malloc(nlength);
+		memcpy(buf[buf_index].data, ppayload, nlength); //패킷 저장
+		buf[buf_index].dev_num = dev_num;
+		buf[buf_index].nlength = nlength;
+		memcpy(buf[buf_index].dest_ip, routerDlg->m_IPLayer->GetDstIP(dev_num),4);
+		buf_index++;
+		buf_index %= 2; //Circular 버퍼
+	} //안비어 있다면 패킷을버림
+
+	arp_message.arp_op = request; //request message
+	memcpy(arp_message.arp_srcprotoaddr, routerDlg->m_IPLayer->GetSrcIP(dev_num), 4); //보내는 사람 ip
+	memcpy(arp_message.arp_srchaddr, ((CEthernetLayer*)this->mp_UnderLayer)->GetSourceAddress(dev_num), 6); //보내는사람 mac
+	memcpy(arp_message.arp_destprotoaddr,routerDlg->m_IPLayer->GetDstIP(dev_num), 4); //받는사람 ip
+	//LP_arpDlg->SetTimer(wait_timer,4000,NULL); //timer 가동
+	return ((CEthernetLayer*)this->mp_UnderLayer)->Send((unsigned char *)&arp_message, ARP_MESSAGE_SIZE, arp_type, dev_num);//arp message
 }
 
 BOOL CARPLayer::Receive(unsigned char* ppayload,int dev_num){
 	LPARP_Message receive_arp_message = (LPARP_Message)ppayload;
-	CRouterDlg * routerDlg =  ((CRouterDlg *)(GetUnderLayer()->GetUpperLayer(0)->GetUpperLayer(0)));
+	CRouterDlg* routerDlg =  ((CRouterDlg *)(GetUnderLayer()->GetUpperLayer(0)->GetUpperLayer(0)));
 	ResetMessage();
+
 	int index;
 	if(receive_arp_message->arp_op == request){ //요구
-		if(memcmp(receive_arp_message->arp_destprotoaddr,routerDlg->m_IPLayer->GetSrcIP(dev_num),4)){// 내 ip가 아닌데 올 경우
+		if(memcmp(receive_arp_message->arp_destprotoaddr, routerDlg->m_IPLayer->GetSrcIP(dev_num), 4)){// 내 ip가 아닌데 올 경우
 			if((index = SearchProxyTable(receive_arp_message->arp_destprotoaddr)) != -1)	{ //proxy Table에 존재할 경우
 				POSITION pos = Proxy_Table.FindIndex(index);
 				PROXY_ENTRY entry = Proxy_Table.GetAt(pos);
 				proxy_arp_message.arp_op = reply;
-				memcpy(proxy_arp_message.arp_desthdaddr,receive_arp_message->arp_srcprotoaddr,4);
-				memcpy(proxy_arp_message.arp_desthdaddr,receive_arp_message->arp_srchaddr,6);
-				memcpy(proxy_arp_message.arp_srchaddr,routerDlg->m_EthernetLayer->GetSourceAddress(dev_num),6);
-				memcpy(proxy_arp_message.arp_srcprotoaddr,entry.Ip_addr,4);	//proxy 값을 넣고 전송시켜줌
-				routerDlg->m_EthernetLayer->Send((unsigned char *)&proxy_arp_message,ARP_MESSAGE_SIZE,arp_type,dev_num);
+				memcpy(proxy_arp_message.arp_desthdaddr, receive_arp_message->arp_srcprotoaddr, 4);
+				memcpy(proxy_arp_message.arp_desthdaddr, receive_arp_message->arp_srchaddr, 6);
+				memcpy(proxy_arp_message.arp_srchaddr, routerDlg->m_EthernetLayer->GetSourceAddress(dev_num), 6);
+				memcpy(proxy_arp_message.arp_srcprotoaddr, entry.Ip_addr, 4);	//proxy 값을 넣고 전송시켜줌
+				routerDlg->m_EthernetLayer->Send((unsigned char *)&proxy_arp_message, ARP_MESSAGE_SIZE, arp_type,dev_num);
 			}
-			else{ //Cache Table에 존재하는지 검사
-				if((index = SearchIpAtTable(receive_arp_message->arp_srcprotoaddr)) != -1){ //cache table에 존재할 경우 갱신
-					//해당 entry를 찾아 값 수정
-					POSITION pos = Cache_Table.FindIndex(index);
-					Cache_Table.GetAt(pos).cache_ttl = 1200;
-					Cache_Table.GetAt(pos).cache_type = complete;
-					memcpy(Cache_Table.GetAt(pos).Mac_addr,receive_arp_message->arp_srchaddr,6);
-					updateCacheTable();
-				}
-				else{ //없을 경우 -- ARP table에 추가
-					LPCACHE_ENTRY Cache_entry;
-					Cache_entry = (LPCACHE_ENTRY)malloc(sizeof(CACHE_ENTRY));
-					memcpy(Cache_entry->Ip_addr,receive_arp_message->arp_srcprotoaddr,4);
-					memcpy(Cache_entry->Mac_addr,receive_arp_message->arp_srchaddr,6);
-					Cache_entry->cache_ttl = 1200; //20분
-					Cache_entry->cache_type = complete;
-					InsertCache(Cache_entry);
-				}
+
+			if((index = SearchIpAtTable(receive_arp_message->arp_srcprotoaddr)) != -1){ //cache table에 존재할 경우 갱신
+				//해당 entry를 찾아 값 수정
+				POSITION pos = Cache_Table.FindIndex(index);
+				Cache_Table.GetAt(pos).cache_ttl = 1200;
+				Cache_Table.GetAt(pos).cache_type = complete;
+				memcpy(Cache_Table.GetAt(pos).Mac_addr, receive_arp_message->arp_srchaddr, 6);
+				updateCacheTable();
+			}else{ //없을 경우 -- ARP table에 추가
+				LPCACHE_ENTRY Cache_entry;
+				Cache_entry = (LPCACHE_ENTRY)malloc(sizeof(CACHE_ENTRY));
+				memcpy(Cache_entry->Ip_addr, receive_arp_message->arp_srcprotoaddr, 4);
+				memcpy(Cache_entry->Mac_addr, receive_arp_message->arp_srchaddr, 6);
+				Cache_entry->cache_ttl = 1200; //20분
+				Cache_entry->cache_type = complete;
+				InsertCache(Cache_entry);
 			}
 		}
 		else{ //자신의 ip로 온 경우
-			if(!memcmp(receive_arp_message->arp_srchaddr,routerDlg->m_EthernetLayer->GetSourceAddress(dev_num),6)){ 
+			if(memcmp(receive_arp_message->arp_srchaddr, routerDlg->m_EthernetLayer->GetSourceAddress(dev_num), 6) == 0){ 
 				//destmac == srcmac일경우(자시자신전송)
 				return FALSE;
 				//자기자신 전송이므로 무시한다.
 			}
-			else{ //아닌경우
-				//ResetMessage(); //Message리셋
-				arp_message.arp_op = reply;
-				memcpy(arp_message.arp_srchaddr,routerDlg->m_EthernetLayer->GetSourceAddress(dev_num),6); //보내는 사람mac주소
-				memcpy(arp_message.arp_srcprotoaddr,routerDlg->m_IPLayer->GetSrcIP(dev_num),4); //보내는 사람ip주소
-				memcpy(arp_message.arp_desthdaddr,receive_arp_message->arp_srchaddr,6); //mac주소
-				memcpy(arp_message.arp_destprotoaddr,receive_arp_message->arp_srcprotoaddr,4); //ip주소
-				if(!memcmp(receive_arp_message->arp_srcprotoaddr,routerDlg->m_IPLayer->GetSrcIP(dev_num),4)){ 
-					//자신의 ip와 보내는 쪽 srcIP가 같은 경우 충돌
-					AfxMessageBox(_T("IP충돌 입니다."),0,0);
-				}
-				else{
-					if(SearchIpAtTable(receive_arp_message->arp_srcprotoaddr) != -1){ //table에존재할 경우
-					}
-					else{ //테이블에 존재하지 않을경우 해당 ip추가
-						//arp_message 설정종료
-						LPCACHE_ENTRY Cache_entry; //table 에 insert할 cache
-						Cache_entry = (LPCACHE_ENTRY)malloc(sizeof(CACHE_ENTRY));
-						memcpy(Cache_entry->Ip_addr,receive_arp_message->arp_srcprotoaddr,4);
-						memcpy(Cache_entry->Mac_addr,receive_arp_message->arp_srchaddr,6);
-						Cache_entry->cache_ttl = 1200; //20분
-						Cache_entry->cache_type = complete;
-						InsertCache(Cache_entry);
-					}
-				}
-				//replay메시지를 보내줌
-				routerDlg->m_EthernetLayer->Send((unsigned char *)&arp_message,ARP_MESSAGE_SIZE,arp_type,dev_num);
+			
+			//srcmac이 다른 컴퓨터일 경우
+			//ResetMessage(); //Message리셋
+			if(!memcmp(receive_arp_message->arp_srcprotoaddr, routerDlg->m_IPLayer->GetSrcIP(dev_num), 4)){ 
+				//자신의 ip와 보내는 쪽 srcIP가 같은 경우 충돌
+				AfxMessageBox(_T("IP충돌 입니다."),0,0);
 			}
+
+			if((index = SearchIpAtTable(receive_arp_message->arp_srcprotoaddr)) != -1){ //table에존재할 경우
+				//해당 entry를 찾아 값 수정
+				POSITION pos = Cache_Table.FindIndex(index);
+				Cache_Table.GetAt(pos).cache_ttl = 1200;
+				Cache_Table.GetAt(pos).cache_type = complete;
+				memcpy(Cache_Table.GetAt(pos).Mac_addr, receive_arp_message->arp_srchaddr, 6);
+				updateCacheTable();
+			}else{ //테이블에 존재하지 않을경우 해당 ip추가
+				//arp_message 설정종료
+				LPCACHE_ENTRY Cache_entry; //table 에 insert할 cache
+				Cache_entry = (LPCACHE_ENTRY)malloc(sizeof(CACHE_ENTRY));
+				memcpy(Cache_entry->Ip_addr, receive_arp_message->arp_srcprotoaddr, 4);
+				memcpy(Cache_entry->Mac_addr, receive_arp_message->arp_srchaddr, 6);
+				Cache_entry->cache_ttl = 1200; //20분
+				Cache_entry->cache_type = complete;
+				InsertCache(Cache_entry);
+			}
+				
+			//replay메시지를 보내줌
+			arp_message.arp_op = reply;
+			memcpy(arp_message.arp_srchaddr, routerDlg->m_EthernetLayer->GetSourceAddress(dev_num), 6); //보내는 사람mac주소
+			memcpy(arp_message.arp_srcprotoaddr, routerDlg->m_IPLayer->GetSrcIP(dev_num), 4); //보내는 사람ip주소
+			memcpy(arp_message.arp_desthdaddr, receive_arp_message->arp_srchaddr, 6); //mac주소
+			memcpy(arp_message.arp_destprotoaddr, receive_arp_message->arp_srcprotoaddr, 4); //ip주소
+			routerDlg->m_EthernetLayer->Send((unsigned char *)&arp_message,ARP_MESSAGE_SIZE,arp_type,dev_num);
 		}
 		return TRUE;
-	} else if(receive_arp_message->arp_op == reply){ //응답
+	}else if(receive_arp_message->arp_op == reply){ //응답
 		if(!memcmp(receive_arp_message->arp_srcprotoaddr,routerDlg->m_IPLayer->GetSrcIP(dev_num),4)){ //자신의 ip = 발송자 ip의경우
 			AfxMessageBox("Ip충돌입니다.",0,0);
-		} else{ //자신의 ip != 발송자 ip
+		}else{ //자신의 ip != 발송자 ip
 			//arp_message 초기화 및 설정
 			LPCACHE_ENTRY Cache_entry; //table 에 insert할 cache
 			Cache_entry = (LPCACHE_ENTRY)malloc(sizeof(CACHE_ENTRY));
@@ -150,6 +159,7 @@ BOOL CARPLayer::Receive(unsigned char* ppayload,int dev_num){
 			memcpy(Cache_entry->Mac_addr,receive_arp_message->arp_srchaddr,6);
 			Cache_entry->cache_ttl = 1200; //20분
 			Cache_entry->cache_type = complete;
+			
 			if((index = SearchIpAtTable(Cache_entry->Ip_addr)) != -1){ //존재할경우 값을교환
 				POSITION pos = Cache_Table.FindIndex(index);
 				LPCACHE_ENTRY entry = &Cache_Table.GetAt(pos);
@@ -157,12 +167,11 @@ BOOL CARPLayer::Receive(unsigned char* ppayload,int dev_num){
 				entry->cache_type = complete;
 				memcpy(entry->Mac_addr,Cache_entry->Mac_addr,6);
 				free(Cache_entry); //메모리 해제
-			}
-			else{ //존재하지 않을경우 테이블에 삽입
+			}else{ //존재하지 않을경우 테이블에 삽입
 				InsertCache(Cache_entry); //cache insert
 			}
 
-			if(buf[out_index].valid == 1 && !memcmp(buf[out_index].dest_ip,receive_arp_message->arp_srcprotoaddr,4)){ 
+			while(buf[out_index].valid == 1 && !memcmp(buf[out_index].dest_ip, receive_arp_message->arp_srcprotoaddr, 4)){ 
 				//버퍼의자료 send
 				routerDlg->m_EthernetLayer->SetDestinAddress(receive_arp_message->arp_srchaddr, dev_num);
 				routerDlg->m_EthernetLayer->Send(buf[out_index].data,buf[out_index].nlength,ip_type,buf[out_index].dev_num);
@@ -171,27 +180,22 @@ BOOL CARPLayer::Receive(unsigned char* ppayload,int dev_num){
 				out_index--;
 				out_index %= 2;
 			}
-
 			return TRUE;
 		}
 	}
 	return FALSE;
 }
 
-int CARPLayer::SearchIpAtTable(unsigned char Ip_addr[4]){ //ip찾는 함수
-	int count;
-	int i;
-	int ret = -1;
+int CARPLayer::SearchIpAtTable(unsigned char Ip_addr[4]){
+	int count, i, ret = -1;
 	CACHE_ENTRY temp;
 
-	if(Cache_Table.GetCount() == 0) {		//Cache table 에 아무것도 없을 경우
-		return -1;
-	} else {								//존재 할 경우
-		count = Cache_Table.GetCount();
-		for(i=0; i<count; i++) {
+	count = Cache_Table.GetCount();
+	if(count != 0) {	//캐시 테이블에 값이 존재 할 경우
+		for(i = 0; i < count; i++) {
 			temp = Cache_Table.GetAt(Cache_Table.FindIndex(i));
-			if(memcmp(Ip_addr ,temp.Ip_addr ,  4) == 0) {
-				ret = i;		// index 값 리턴
+			if(memcmp(Ip_addr, temp.Ip_addr, 4) == 0) {
+				ret = i;
 			}
 		}
 	}
@@ -199,21 +203,20 @@ int CARPLayer::SearchIpAtTable(unsigned char Ip_addr[4]){ //ip찾는 함수
 }
 
 int CARPLayer::SearchProxyTable(unsigned char Ip_addr[4]){
-	int count;
-	int i;
+	int count, i, ret = -1;
 	PROXY_ENTRY temp;
-	if(Proxy_Table.GetCount() == 0) {		//Proxy table 에 아무것도 없을 경우
-		return -1;
-	} else {								//존재 할 경우
+
+	count = Proxy_Table.GetCount();
+	if(count != 0){								//존재 할 경우
 		count = Proxy_Table.GetCount();
-		for(i=0; i<count; i++) {
+		for(i = 0; i < count; i++) {
 			temp = Proxy_Table.GetAt(Proxy_Table.FindIndex(i));
 			if(memcmp(Ip_addr ,temp.Ip_addr ,  4) == 0) {
-				return i;		// index 값 리턴
+				ret = i;		// index 값 리턴
 			}
 		}
 	}
-	return -1;	
+	return ret;	
 }
 
 BOOL CARPLayer::InsertCache(LPCACHE_ENTRY Cache_entry){
@@ -260,10 +263,10 @@ BOOL CARPLayer::ResetMessage(){
 	arp_message.arp_hdlength = 0x06;
 	arp_message.arp_protolength = 0x04;
 	arp_message.arp_op = htons(0x0000); //2개로 나뉨
-	memset(arp_message.arp_srchaddr,0,6);
-	memset(arp_message.arp_srcprotoaddr,0,4);
-	memset(arp_message.arp_destprotoaddr,0,4);
-	memset(arp_message.arp_desthdaddr,0,6);
+	memset(arp_message.arp_srchaddr, 0, 6);
+	memset(arp_message.arp_srcprotoaddr, 0, 4);
+	memset(arp_message.arp_destprotoaddr, 0, 4);
+	memset(arp_message.arp_desthdaddr, 0, 6);
 	return TRUE;
 }
 
@@ -273,10 +276,10 @@ BOOL CARPLayer::ResetMessageProxy(){
 	proxy_arp_message.arp_hdlength = 0x06;
 	proxy_arp_message.arp_protolength = 0x04;
 	proxy_arp_message.arp_op = htons(0x0000); //2개로 나뉨
-	memset(proxy_arp_message.arp_srchaddr,0,6);
-	memset(proxy_arp_message.arp_srcprotoaddr,0,4);
-	memset(proxy_arp_message.arp_destprotoaddr,0,4);
-	memset(proxy_arp_message.arp_desthdaddr,0,6);
+	memset(proxy_arp_message.arp_srchaddr, 0, 6);
+	memset(proxy_arp_message.arp_srcprotoaddr, 0, 4);
+	memset(proxy_arp_message.arp_destprotoaddr, 0, 4);
+	memset(proxy_arp_message.arp_desthdaddr, 0, 6);
 	return TRUE;
 }
 
@@ -286,14 +289,14 @@ void CARPLayer::updateCacheTable(){ //케쉬 테이블 업데이트 함수
 	CString ip,mac,time,type;
 	POSITION index;
 	CACHE_ENTRY entry; //head position
-	for(int i=0;i<Cache_Table.GetCount();i++){ //케쉬 테이블 마지막까지
+	for(int i = 0; i < Cache_Table.GetCount(); i++){ //케쉬 테이블 마지막까지
 		index = Cache_Table.FindIndex(i);
 		entry = Cache_Table.GetAt(index);
-		ip.Format("%d.%d.%d.%d",entry.Ip_addr[0],entry.Ip_addr[1],entry.Ip_addr[2],entry.Ip_addr[3]);
-		mac.Format("%x-%x-%x-%x-%x-%x",entry.Mac_addr[0],entry.Mac_addr[1],entry.Mac_addr[2],entry.Mac_addr[3],
-			entry.Mac_addr[4],entry.Mac_addr[5]);
+		ip.Format("%d.%d.%d.%d", entry.Ip_addr[0], entry.Ip_addr[1], entry.Ip_addr[2], entry.Ip_addr[3]);
+		mac.Format("%x-%x-%x-%x-%x-%x", entry.Mac_addr[0], entry.Mac_addr[1], entry.Mac_addr[2], entry.Mac_addr[3],
+			entry.Mac_addr[4], entry.Mac_addr[5]);
 		(entry.cache_type == complete ? type.Format("Complete") : type.Format("Incomplete"));
-		time.Format("%d:%d",entry.cache_ttl/60,entry.cache_ttl%60);
+		time.Format("%d:%d",entry.cache_ttl/60, entry.cache_ttl%60);
 		routerDlg->ListBox_ARPCacheTable.InsertItem(i,ip);
 		routerDlg->ListBox_ARPCacheTable.SetItem(i,1,LVIF_TEXT,mac,0,0,0,NULL);
 		routerDlg->ListBox_ARPCacheTable.SetItem(i,2,LVIF_TEXT,type,0,0,0,NULL);
@@ -303,12 +306,12 @@ void CARPLayer::updateCacheTable(){ //케쉬 테이블 업데이트 함수
 }
 
 void CARPLayer::updateProxyTable(){		
-	CRouterDlg * routerDlg =  ((CRouterDlg *)(GetUnderLayer()->GetUpperLayer(0)->GetUpperLayer(0)));
+	CRouterDlg* routerDlg =  ((CRouterDlg *)(GetUnderLayer()->GetUpperLayer(0)->GetUpperLayer(0)));
 	routerDlg->ListBox_ARPProxyTable.DeleteAllItems();
-	CString ip,mac,name;
+	CString ip, mac, name;
 	POSITION index1;
 	PROXY_ENTRY entry1; //head position
-	for(int i=0;i<Proxy_Table.GetCount();i++){
+	for(int i = 0;i < Proxy_Table.GetCount(); i++){
 		index1 = Proxy_Table.FindIndex(i);
 		entry1 = Proxy_Table.GetAt(index1);
 		ip.Format("%d.%d.%d.%d",entry1.Ip_addr[0],entry1.Ip_addr[1],entry1.Ip_addr[2],entry1.Ip_addr[3]);
@@ -322,11 +325,10 @@ void CARPLayer::updateProxyTable(){
 	routerDlg->ListBox_ARPProxyTable.UpdateWindow();
 }
 
-
 void CARPLayer::decreaseTime(){
 	if(!Cache_Table.IsEmpty()){
 		POSITION index;
-		for(int i=0;i<Cache_Table.GetCount();i++){
+		for(int i = 0; i < Cache_Table.GetCount(); i++){
 			index = Cache_Table.FindIndex(i);
 			unsigned short ttl = Cache_Table.GetAt(index).cache_ttl -= 5;
 			if(ttl <= 0){// 삭제
